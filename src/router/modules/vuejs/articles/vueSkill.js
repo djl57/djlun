27e1390bb369e42b6e3b3a5cbd0ab27da5e64f0c
djl57 <18367345931@163.com>
2019-03-28 14:58:41 +0800

    co

--- src/router/modules/vuejs/articles/vueSkill.js
+++ src/router/modules/vuejs/articles/vueSkill.js
@@ -1,22 +1,29 @@
-const vueSkill = [
-  {
-    path: '/keepaliveUsing',
-    name: 'keepaliveUsing',
+const vueSkill = []
+const data = [
+  { name: 'keepaliveUsing', title: '动态使用keepalive' },
+  { name: 'elscrollbar', title: '使用element的el-scrollbar' },
+]
+data.forEach(el => {
+  vueSkill.push({
+    path: `/${el.name}`,
+    name: `${el.name}`,
     component: () => import('@/components/base'),
     meta: {
-      title: 'keep-alive动态使用',
+      title: `${el.title}`,
       type: 'vueSkill'
     },
-    redirect: '/keepaliveUsing/keepaliveUsingIndex',
+    redirect: `/${el.name}/${el.name}Index`,
     children: [
       {
-        path: 'keepaliveUsingIndex',
-        name: 'keepaliveUsingIndex',
-        component: () => import('@/views/vuejs/vueSkill/keepaliveUsing')
+        path: `${el.name}Index`,
+        name: `${el.name}Index`,
+        component: () => import(`@/views/vuejs/vueSkill/${el.name}`)
       }
     ]
-  },
-]
+  })
+})
 export {
   vueSkill
 }
+
+
---
+++ src/views/vuejs/vueSkill/elscrollbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <article class="app-box">
+    <h2>不是按需加载element时</h2>
+    <pre><code>{{ notanxu }}</code></pre>
+
+    <h2>按需加载element时</h2>
+    <p>先加载，再跟上面一样使用</p>
+    <pre><code>{{ anxu }}</code></pre>
+
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'elscrollbar',
+  data() {
+    return {
+      createTime: '2019-03-27 13:25:00',
+      notanxu: `<el-scrollbar wrap-class="" 
+  wrap-style="color: red;" 
+  view-style="font-weight: bold;" 
+  view-class="view-box" 
+  :native="false"
+  :vertical="false"[有了这个在横向滚动时会让出原生的竖向滚动条的位置]></el-scrollbar>`,
+      anxu: `import { Scrollbar } from 'element-ui';
+Vue.use(Scrollbar);`
+    }
+  },
+  components: {
+    
+  },
+  mounted() {
+    
+  },
+  computed: {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
